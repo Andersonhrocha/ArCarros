3c68ce6b8c631cf12f5c30784c5d1f1a226fe75d
Anderson <Anderson@AndersonRocha.mshome.net>
2017-10-23 13:53:39 -0300

    Criação das classes ModelCarro e DaoCarro com seus métodos

---
+++ src/java/dao/DaoCarro.java
@@ -0,0 +1,170 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.ModelCarro;
+
+/**
+ *
+ * @author Anderson
+ */
+public class DaoCarro extends ModuloConexao {
+
+    //VARIÁVEIS
+    public Connection conexao;
+    private String sql = "";
+
+    //CONSTRUTOR PRINCIPAL DA CLASSE
+    public DaoCarro() {
+        conexao = this.abrirConexao();
+    }
+
+    //MÉTODO PARA INCLUSÃO
+    public void incluir(ModelCarro pro) { //opcao 1.
+
+        sql = "INSERT INTO carro (id_carro, ano, cor, cpf_cliente, id_modelo) VALUES (?,?,?,?,?)";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, pro.getId_carro());
+            pst.setString(2, pro.getAno());
+            pst.setString(3, pro.getCor());
+            pst.setString(4, pro.getRelacao_id_cliente().getCpf_cliente());
+            pst.setInt(5, pro.getRelacao_id_modelo().getId_modelo());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro inserido com sucesso.");
+            } else {
+                System.out.println("Registro não foi inserido.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoCarro.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao inserir um carro", ex);
+        }
+    }//FIM DA CLASSE incluir
+
+    //MÉTODO PARA EDIÇÃO
+    public void editar(ModelCarro pro) { //opcao 2.
+
+        sql = "UPDATE carro SET ano=?, cor=?, id_modelo=? WHERE id_carro=? ";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, pro.getAno());
+            pst.setString(2, pro.getCor());
+            pst.setInt(3, pro.getRelacao_id_modelo().getId_modelo());
+            pst.setString(4, pro.getId_carro());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro alterado com sucesso.");
+            } else {
+                System.out.println("Registro não existe para ser alterado.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoCarro.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao atualizar o registro de um carro", ex);
+        }
+
+    }//FIM DA CLASSE editar
+
+    //MÉTODO PARA EXCLUSÃO
+    public void excluir(ModelCarro pro) {// opcao 3.
+
+        sql = "DELETE FROM carro WHERE id_carro=? ";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, pro.getId_carro());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro excluido com sucesso.");
+            } else {
+                System.out.println("Registro não existente para exclusão.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoCarro.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao excluir um carro.", ex);
+        }
+    }//FIM DA CLASSE excluir
+
+    //MÉTODO PARA BUSCAR POR ID
+    public ModelCarro buscar(ModelCarro pro) { //opcao 4.
+
+        sql = "SELECT * FROM carro WHERE id_carro = ?";
+
+        ModelCarro retorno = null;
+
+        try {
+
+            PreparedStatement pst = conexao.prepareStatement(sql);
+
+            pst.setString(1, pro.getId_carro());
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+
+                retorno = new ModelCarro();
+                retorno.setId_carro(rs.getString("id_carro"));
+                retorno.setAno(rs.getString("ano"));
+                retorno.setCor(rs.getString("cor"));
+                retorno.getRelacao_id_cliente().setCpf_cliente(rs.getString("cpf_cliente"));
+                retorno.getRelacao_id_modelo().setId_modelo(Integer.parseInt(rs.getString("id_modelo")));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoCarro.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Uma falha ocorreu ao buscar um carro.", ex);
+        }
+        return retorno;
+    }//FIM DA CLASSE buscar
+
+    //MÉTODO PARA LISTAGEM DE TODOS OS DADOS
+    public List<ModelCarro> listarTodos() { //opcao 5.
+
+         // SELECT UNINDO AS TABELAS CARRO E MODELO
+        sql = "SELECT carro.id_carro, carro.ano, carro.cor, carro.cpf_cliente, modelo.id_modelo, modelo.nome, modelo.motorizacao FROM carro INNER JOIN modelo ON carro.id_modelo = modelo.id_modelo";
+        
+        List<ModelCarro> lista = new ArrayList<ModelCarro>();
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                ModelCarro item = new ModelCarro();
+                item.setId_carro(rs.getString("id_carro"));
+                item.setAno(rs.getString("ano"));
+                item.setCor(rs.getString("cor"));
+                item.getRelacao_id_cliente().setCpf_cliente(rs.getString("cpf_cliente"));
+                item.getRelacao_id_modelo().setId_modelo(Integer.parseInt(rs.getString("id_modelo")));
+                item.getRelacao_id_modelo().setNome_modelo(rs.getString("nome_modelo"));
+                item.getRelacao_id_modelo().setMotorizacao(rs.getString("motorizacao"));
+
+                lista.add(item);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoCarro.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Uma falha ocorreu ao tentar listar todas os carros.", ex);
+        }
+        return lista;
+
+    } //FIM DA CLASSE listarTodos
+
+}
---
+++ src/java/model/ModelCarro.java
@@ -0,0 +1,71 @@
+package model;
+
+/**
+ *
+ * @author Anderson
+ */
+public class ModelCarro {
+
+    private String id_carro;
+    private String ano;
+    private String cor;
+    private ModelCliente relacao_id_cliente; //AGREGAÇÃO/RELACIONAMENTO COM A CLASSE MODELCLIENTE
+    private ModelModelo relacao_id_modelo; //AGREGAÇÃO/RELACIONAMENTO COM A CLASSE MODELMODELO
+
+    //CONSTRUTOR PRINCIPAL INICIANDO AS VARIÁVEIS E CRIANDO OBJETOS DAS ENTIDADES MODELCLIENTE E MODELMOLETO    
+    public ModelCarro() {
+        ano = "";
+        cor = "";
+        relacao_id_cliente = new ModelCliente();
+        relacao_id_modelo = new ModelModelo();
+    }
+
+    public String getId_carro() {
+        return id_carro;
+    }
+
+    public void setId_carro(String id_carro) {
+        this.id_carro = id_carro;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public ModelCliente getRelacao_id_cliente() {
+        return relacao_id_cliente;
+    }
+
+    public void setRelacao_id_cliente(ModelCliente relacao_id_cliente) {
+        this.relacao_id_cliente = relacao_id_cliente;
+    }
+
+    public ModelModelo getRelacao_id_modelo() {
+        return relacao_id_modelo;
+    }
+
+    public void setRelacao_id_modelo(ModelModelo relacao_id_modelo) {
+        this.relacao_id_modelo = relacao_id_modelo;
+    }
+
+    public ModelCarro(String id_carro, String ano, String cor, ModelCliente relacao_id_cliente, ModelModelo relacao_id_modelo) {
+        this.id_carro = id_carro;
+        this.ano = ano;
+        this.cor = cor;
+        this.relacao_id_cliente = relacao_id_cliente;
+        this.relacao_id_modelo = relacao_id_modelo;
+    }
+
+}
