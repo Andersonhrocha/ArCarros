ef9432e43e7090238f77d1dc90d70f34ed87a66c
Anderson <Anderson@AndersonRocha.mshome.net>
2017-10-27 16:31:07 -0300

    Criação das classes ModelProduto e DaoProduto com seus métodos

---
+++ src/java/dao/DaoProduto.java
@@ -0,0 +1,203 @@
+package dao;
+
+/**
+ *
+ * @author Anderson
+ */
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.ModelProduto;
+
+public class DaoProduto extends ModuloConexao {
+
+    //VARIÁVEIS
+    public Connection conexao;
+    private String sql = "";
+
+    //CONSTRUTOR PRINCIPAL DA CLASSE
+    public DaoProduto() {
+        conexao = this.abrirConexao();
+    }
+
+    //MÉTODO PARA INCLUSÃO
+    public void incluir(ModelProduto pro) { //opcao 1.
+
+        sql = "INSERT INTO produto (id_produto, nome_produto, descricao_produto, validade, id_categoria, valor, qtd_estoque) VALUES (?,?,?,?,?,?,?)";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, pro.getId_produto());
+            pst.setString(2, pro.getNome_produto());
+            pst.setString(3, pro.getDescricao_produto());
+            pst.setDate(4, new Date(pro.getValidade().getTime()));
+            pst.setInt(5, pro.getRelacao_id_categoria().getId_categoria());
+            pst.setDouble(6, pro.getValor());
+            pst.setInt(7, pro.getQtd_estoque());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro inserido com sucesso.");
+            } else {
+                System.out.println("Registro não foi inserido.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduto.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao inserir o produto", ex);
+        }
+    }//FIM DA CLASSE incluir
+
+    //MÉTODO PARA EDIÇÃO
+    public void editar(ModelProduto pro) { //opcao 2.
+
+        sql = "UPDATE produto SET nome_produto=?, descricao_produto=?, validade=?, id_categoria=?, valor=?, qtd_estoque=? WHERE id_produto=? ";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+
+            pst.setString(1, pro.getNome_produto());
+            pst.setString(2, pro.getDescricao_produto());
+            pst.setDate(3, new Date(pro.getValidade().getTime()));
+            pst.setInt(4, pro.getRelacao_id_categoria().getId_categoria());
+            pst.setDouble(5, pro.getValor());
+            pst.setInt(6, pro.getQtd_estoque());
+            pst.setString(7, pro.getId_produto());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro alterado com sucesso.");
+            } else {
+                System.out.println("Registro não existe para ser alterado.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduto.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao atualizar o registro do produto", ex);
+        }
+
+    }//FIM DA CLASSE editar
+
+    //MÉTODO PARA EXCLUSÃO
+    public void excluir(ModelProduto pro) {// opcao 3.
+
+        sql = "DELETE FROM produto WHERE id_produto=? ";
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            pst.setString(1, pro.getId_produto());
+
+            int status = pst.executeUpdate();
+            if (status > 0) {
+                System.out.println("Registro excluido com sucesso.");
+            } else {
+                System.out.println("Registro não existente para exclusão.");
+            }
+            pst.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduto.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Falha ao excluir um produto.", ex);
+        }
+    }//FIM DA CLASSE excluir
+
+    //MÉTODO PARA BUSCAR POR ID
+    public ModelProduto buscar(ModelProduto pro) { //opcao 4.
+
+        sql = "SELECT * FROM produto WHERE id_produto = ?";
+
+        ModelProduto retorno = null;
+
+        try {
+
+            PreparedStatement pst = conexao.prepareStatement(sql);
+
+            pst.setString(1, pro.getId_produto());
+            ResultSet rs = pst.executeQuery();
+
+            if (rs.next()) {
+
+                retorno = new ModelProduto();
+                retorno.setId_produto(rs.getString("id_produto"));
+                retorno.setNome_produto(rs.getString("nome_produto"));
+                retorno.setDescricao_produto(rs.getString("descricao_produto"));
+                retorno.setValidade(rs.getDate("validade"));
+                retorno.getRelacao_id_categoria().setId_categoria(Integer.parseInt(rs.getString("id_categoria")));
+                retorno.setValor(Double.valueOf(rs.getString("valor")));
+                retorno.setQtd_estoque(Integer.parseInt(rs.getString("qtd_estoque")));
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduto.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Uma falha ocorreu ao buscar um produto.", ex);
+        }
+        return retorno;
+    }//FIM DA CLASSE buscar
+
+    //MÉTODO PARA LISTAGEM DE TODOS OS DADOS
+    public List<ModelProduto> listarTodos() { //opcao 5.
+
+        sql = "SELECT * FROM produto";
+
+        List<ModelProduto> lista = new ArrayList<ModelProduto>();
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                ModelProduto item = new ModelProduto();
+                item.setId_produto(rs.getString("id_produto"));
+                item.setNome_produto(rs.getString("nome_produto"));
+                item.setDescricao_produto(rs.getString("descricao_produto"));
+                item.setValor(Double.valueOf(rs.getString("valor")));
+
+                lista.add(item);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoProduto.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Uma falha ocorreu ao tentar listar todos produto.", ex);
+        }
+        return lista;
+
+    } //FIM DA CLASSE listarTodos
+
+    //MÉTODO PARA LISTAGEM DE TODOS OS PRODUTOS
+    public List<ModelProduto> listarProdutos() { //opcao 6.
+
+        sql = " SELECT * FROM produto ";
+
+        List<ModelProduto> lista = new ArrayList<ModelProduto>();
+
+        try {
+            PreparedStatement pst = conexao.prepareStatement(sql);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+
+                ModelProduto item = new ModelProduto();
+                item.setId_produto(rs.getString("id_produto"));
+                item.setNome_produto(rs.getString("nome_produto"));
+
+                lista.add(item);
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DaoModelo.class.getName()).log(Level.SEVERE, null, ex);
+            throw new RuntimeException("Uma falha ocorreu ao tentar listar todas as marcas.", ex);
+        }
+        return lista;
+
+    } //FIM DA CLASSE listarProdutos
+
+}
---
+++ src/java/model/ModelProduto.java
@@ -0,0 +1,82 @@
+package model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Anderson
+ */
+public class ModelProduto {
+
+    private String id_produto;
+    private String nome_produto;
+    private String descricao_produto;
+    private Date validade;
+    private ModelCategoria relacao_id_categoria; //AGREGAÇÃO/RELACIONAMENTO COM A CLASSE MODELCATEGORIA
+    private Double valor;
+    private int qtd_estoque;
+
+    //CONSTRUTOR PRINCIPAL CRIANDO OBJETO DA ENTIDADE MODELCATEGORIA  
+    public ModelProduto() {
+
+        relacao_id_categoria = new ModelCategoria();
+
+    }
+
+    public String getId_produto() {
+        return id_produto;
+    }
+
+    public void setId_produto(String id_produto) {
+        this.id_produto = id_produto;
+    }
+
+    public String getNome_produto() {
+        return nome_produto;
+    }
+
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
+
+    public String getDescricao_produto() {
+        return descricao_produto;
+    }
+
+    public void setDescricao_produto(String descricao_produto) {
+        this.descricao_produto = descricao_produto;
+    }
+
+    public Date getValidade() {
+        return validade;
+    }
+
+    public void setValidade(Date validade) {
+        this.validade = validade;
+    }
+
+    public ModelCategoria getRelacao_id_categoria() {
+        return relacao_id_categoria;
+    }
+
+    public void setRelacao_id_categoria(ModelCategoria relacao_id_categoria) {
+        this.relacao_id_categoria = relacao_id_categoria;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public int getQtd_estoque() {
+        return qtd_estoque;
+    }
+
+    public void setQtd_estoque(int qtd_estoque) {
+        this.qtd_estoque = qtd_estoque;
+    }
+
+}
