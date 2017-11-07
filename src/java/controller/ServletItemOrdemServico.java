75c22a0ca2689f988585317a8e3fcdb37af935df
Anderson <Anderson@AndersonRocha.mshome.net>
2017-11-07 13:20:47 -0300

    Criação da classe controller ItemOrdemServico

---
+++ src/java/controller/ServletItemOrdemServico.java
@@ -0,0 +1,120 @@
+package controller;
+
+import dao.DaoItemOrdemServico;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.ModelItemOrdemServico;
+
+/**
+ *
+ * @author Anderson
+ */
+@WebServlet(name = "ServletItemOrdemServico", urlPatterns = {"/ServletItemOrdemServico"})
+public class ServletItemOrdemServico extends ServletAbstrato {
+
+    //CONSTANTES DAS PÁGINAS .JSP
+    private static final String ADICIONAR_ITEM_ORDEM_SERVICO = "adicionarItemOrdemServico.jsp";
+    private static final String EDITAR_ITEM_ORDEM_SERVICO = "editarOrdemServico.jsp";
+    private static final String LISTAR_ITEM_ORDEM_SERVICO = "listarItemOrdemServico.jsp";
+    private final DaoItemOrdemServico dao;
+
+    //CONSTRUTOR PRINCIPAL
+    public ServletItemOrdemServico() {
+        dao = new DaoItemOrdemServico();
+    }
+
+    @Override
+    public void executar(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+
+             //CRIANDO OBJETO DA CLASSE MODELITEMORDEMSERVICO
+            ModelItemOrdemServico pro = new ModelItemOrdemServico();
+
+            //RECEBE PARAMETRO VIA POST DO NAVEGADOR
+            String acao = request.getParameter("acao");
+
+            //INICIO DOS MÉTODOS DO BANCO DE DADOS
+            if (acao.equalsIgnoreCase("inserir")) {
+
+                //ARMAZENANDO PARAMETRO PREDEFINIDO NO BANCO DE DADOS
+                pro.getRelacao_id_ordem_servico().setId_ordem_Servico(Integer.parseInt(request.getParameter("txtOrdemServico")));
+                pro.getRelacao_id_produto().setId_produto(request.getParameter("txtProduto"));
+                pro.getRelacao_id_servico().setId_servico(Integer.parseInt(request.getParameter("txtServico")));
+                pro.setQtd_prod_utilizado(Integer.parseInt(request.getParameter("txtQtdPro")));
+
+                //INCLUIR NO BANCO DE DADOS
+                dao.incluir(pro);
+
+                //ATRIBUTO COM MENSAGEM DE RETORNO
+                request.setAttribute("mensagem", "Registro efetuado com sucesso.");
+
+                //REDIRECIONAMENTO
+                redirecionarPagina(request, response, ADICIONAR_ITEM_ORDEM_SERVICO);
+
+            } else if (acao.equalsIgnoreCase("editar")) {
+
+                //PARAMETRO QUE OBTEM O ID PARA EDITAR
+                pro.getRelacao_id_ordem_servico().setId_ordem_Servico(Integer.parseInt(request.getParameter("txtOrdemServico")));
+                pro.getRelacao_id_produto().setId_produto(request.getParameter("txtProduto"));
+                pro.getRelacao_id_servico().setId_servico(Integer.parseInt(request.getParameter("txtServico")));
+                pro.setQtd_prod_utilizado(Integer.parseInt(request.getParameter("txtQtdPro")));
+
+                //EDITANDO NO BANCO DE DADOS
+                dao.editar(pro);
+
+                //REDIRECIONAMENTO
+                this.redirecionarPagina(request, response, LISTAR_ITEM_ORDEM_SERVICO);
+
+            } else if (acao.equalsIgnoreCase("excluir")) {
+
+                //PARAMETRO QUE OBTÉM O ID PARA EXCLUSÃO
+                 pro.setId_item_ordem_servico(Integer.parseInt(request.getParameter("txtItemOrdemServico")));
+
+                //EXCLUIDO DO BANCO DE DADOS
+                dao.excluir(pro);
+
+                //REDIRECIONAMENTO
+                this.redirecionarPagina(request, response, LISTAR_ITEM_ORDEM_SERVICO);
+
+            } else if (acao.equalsIgnoreCase("buscar")) {
+
+                //PARAMETRO QUE OBTÉM O ID PARA BUSCAR REGISTRO
+                pro.getRelacao_id_ordem_servico().setId_ordem_Servico(Integer.parseInt(request.getParameter("txtOrdemServico")));
+
+                //BUSCA PARA EDITAR
+                pro = dao.buscar(pro);
+                request.setAttribute("cliente", pro);
+
+                //REDIRECIONAMENTO
+                this.redirecionarPagina(request, response, this.ADICIONAR_ITEM_ORDEM_SERVICO);
+
+            } else if (acao.equalsIgnoreCase("listar")) {
+
+                //REDIRECIONAMENTO
+                this.redirecionarPagina(request, response, this.LISTAR_ITEM_ORDEM_SERVICO);
+            }
+
+        } catch (IOException | ServletException ex) {
+            System.out.println("Erro na interação: " + ex.getMessage());
+        }
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        executar(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        executar(request, response);
+    }
+
+}
