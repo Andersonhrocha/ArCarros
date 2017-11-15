af9341835bf50e535b16690b11e4b7fcb235d3e2
Anderson <Anderson@AndersonRocha.mshome.net>
2017-11-15 14:35:44 -0300

    Criando relatório venda de produto

--- src/java/Relatorio/RelatorioImpresso.java
+++ src/java/Relatorio/RelatorioImpresso.java
@@ -14,10 +14,6 @@ import java.sql.Connection;
 import java.util.HashMap;
 import net.sf.jasperreports.engine.JRException;
 
-/**
- *
- * @author Anderson
- */
 @WebServlet(name = "RelatorioImpresso", urlPatterns = {"/RelatorioImpresso"})
 public class RelatorioImpresso extends HttpServlet {
 
@@ -59,7 +55,7 @@ public class RelatorioImpresso extends HttpServlet {
             } catch (IOException | JRException ex) {
                 System.out.println("Erro ao gerar relatório=>" + ex.getMessage());
             }
-            
+
         } else if (comando.equalsIgnoreCase("imprimir_ordens_servico")) { //LISTA TODAS OS ORDENS DE SERVIÇO PARA IMPRIMIR
 
             try {
@@ -101,10 +97,10 @@ public class RelatorioImpresso extends HttpServlet {
                 conexao = conn.abrirConexao();
 
                 //VAVIÁVEL QUE RECEBE O CAMINHO DO ARQUIVO .JASPER
-                File ComParametros = new File(getServletConfig().getServletContext().getRealPath("/WEB-INF/Relatorio/ItemOrdemServico.jasper"));
+                File caminhoRelatorio = new File(getServletConfig().getServletContext().getRealPath("/WEB-INF/Relatorio/ItemOrdemServico.jasper"));
 
                 //GERENCIADOR DO JASPER PARA CRIA O RELATÓRIO EM PDF
-                bytes = JasperRunManager.runReportToPdf(ComParametros.getPath(), valorParametro, conexao);
+                bytes = JasperRunManager.runReportToPdf(caminhoRelatorio.getPath(), valorParametro, conexao);
 
                 //ESCREVENDO NA SAIDA DO RESPONSE
                 response.setContentType("application/pdf");
@@ -119,46 +115,54 @@ public class RelatorioImpresso extends HttpServlet {
             } catch (IOException | JRException ex) {
                 System.out.println("Erro ao gerar relatório=>" + ex.getMessage());
             }
+
+        } else if (comando.equalsIgnoreCase("imprimir_venda_produto")) {  //IMPRIME VENDA DE PRODUTOS UTILIZANDO O CÓDIGO DA 'VENDA'
+
+            //RECEBE PARAMETRO VIA POST DO FORMULÁRIO UTILIZANDO CAMPO hidden
+            String codigoProjeto = request.getParameter("codigoVendaProduto");
+
+            //PARAMETRO QUE RECEBE O VALOR codVenPro DA SQL DO ARQUIVO COMPILADO .JASPER
+            HashMap valorParametro = new HashMap();
+            valorParametro.put("codVenPro", codigoProjeto);
+
+            try {
+
+                //ABRINDO CONEXÃO
+                conexao = conn.abrirConexao();
+
+                //VAVIÁVEL QUE RECEBE O CAMINHO DO ARQUIVO .JASPER
+                File caminhoRelatorio = new File(getServletConfig().getServletContext().getRealPath("/WEB-INF/Relatorio/VendaProduto.jasper"));
+
+                //GERENCIADOR DO JASPER PARA CRIA O RELATÓRIO EM PDF
+                bytes = JasperRunManager.runReportToPdf(caminhoRelatorio.getPath(), valorParametro, conexao);
+
+                //ESCREVENDO NA SAIDA DO RESPONSE
+                response.setContentType("application/pdf");
+                response.setContentLength(bytes.length);
+                ServletOutputStream ouputStream = response.getOutputStream();
+                ouputStream.write(bytes, 0, bytes.length);
+
+                //EVIANDO CONTEÚDO
+                ouputStream.flush();
+
+                System.out.println("Relatório de venda de produto gerado com Sucesso!");
+            } catch (IOException | JRException ex) {
+                System.out.println("Erro ao gerar relatório=>" + ex.getMessage());
+            }
         }
+
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         gerandoRelatorios(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         gerandoRelatorios(request, response);
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
